336d999e50addc63e338ef7e223920a9e50e07b2
s235216 <162142772+s235216@users.noreply.github.com>
2024-04-29 13:37:29 +0200

    finish leave

--- src/main/java/domain/Leave.java
+++ src/main/java/domain/Leave.java
@@ -4,9 +4,13 @@ import java.util.Calendar;
 
 public class Leave extends PeriodEvent {
 
+    private Calendar startDate;
+    private Calendar deadline;
 
-    public Leave (String name) {
+    public Leave (String name, Calendar startDate, Calendar endDate) {
         super(name);
+        this.startDate = startDate;
+        this.deadline = endDate;
     }
 
 
