658e1cf273377c42460d321e2dea7fb23033e84a
NiklasLysdal <s235235@dtu.dk>
2024-04-29 19:18:14 +0200

    Changed from arrays to arraylists in quicksort implementation

--- src/test/java/example/cucumber/WhiteBox/QuickSortUserCounts.java
+++ src/test/java/example/cucumber/WhiteBox/QuickSortUserCounts.java
@@ -4,6 +4,7 @@ import domain.UserCount;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public abstract class QuickSortUserCounts {
     public static ArrayList<UserCount> sortUsers(ArrayList<UserCount> list ) {
@@ -15,7 +16,7 @@ public abstract class QuickSortUserCounts {
         ArrayList<UserCount> sortedList = new ArrayList<>(Arrays.asList(tempList));
         return sortedList;
     }
-    private static void quickSort (UserCount[] ucArr,int begin, int end) {
+    private static void quickSort (ArrayList<UserCount> ucArr,int begin, int end) {
         if (begin<end) {
             int pivot = partition(ucArr,begin,end); //define pivot element
             quickSort(ucArr,begin,pivot-1);
@@ -28,17 +29,17 @@ public abstract class QuickSortUserCounts {
         ucArr[index2]=tempU;
     }
 
-    private static int partition(UserCount[] ucArr, int begin, int end) {
+    private static int partition(ArrayList<UserCount> ucArr, int begin, int end) {
         int leqIndex = begin-1; //correctPlace of pivot in list.
-        int pivotValue=ucArr[end].getCount(); //store value to compare to
+        int pivotValue=ucArr.get(end).getCount(); //store value to compare to
 
         for (int check = begin;check<end;check++) {
-            if (ucArr[check].getCount()<=pivotValue) {
+            if (ucArr.get(check).getCount()<=pivotValue) {
                 leqIndex++; //move rightmost smallest value index
-                swap(ucArr,leqIndex,check); // swap checkValue into rightMost position of smaller than section
+                ucArr.swap(leqIndex,check); // swap checkValue into rightMost position of smaller than section
             }
         }
-        swap(ucArr,leqIndex+1,end); //swap pivot into
+        Collections.swap(ucArr,leqIndex+1,end); //swap pivot into
         return (leqIndex+1); //return position of pivot
     }
 }
